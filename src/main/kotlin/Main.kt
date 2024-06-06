e3e97aa9705f18494e160ce51a16eb4b1102ff30
Avery-Levin <88396486+Avery-Levin@users.noreply.github.com>
2024-06-06 15:44:17 -0400

    The start of something potentially funny

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+package org.example
+import net.jacobpeterson.alpaca.*
+import net.jacobpeterson.alpaca.model.util.apitype.MarketDataWebsocketSourceType
+import net.jacobpeterson.alpaca.model.util.apitype.TraderAPIEndpointType
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val keyID : String = "PKEEFRHT69BEQOGJ0JQE"
+    val secretKey : String = "8grfwhcgzNXfoHK437NybDwkFdqEg3zB1YPEH2Y6"
+    val endpointType = TraderAPIEndpointType.PAPER
+    val sourceType = MarketDataWebsocketSourceType.IEX
+    val alpacaAPI = AlpacaAPI(keyID, secretKey, endpointType, sourceType)
+    var selectableStocks = listOf("AAPL","ABBV","ABT","ACN","AGN","AIG","ALL","AMGN","AMZN,AXP","BA","BAC","BIIB","BK","BLK","BMY","C","CAT","CELG","CL","CMCSA","COF","COP","COST","CSCO","CVS","CVX","DD","DHR","DIS","DOW","DUK","EMC"
+        ,"EMR","EXC","F","FB","FDX","FOX","FOXA","GD","GE","GILD","GM","GOOG","GOOGL","GS","HAL","HD","HON","IBM","INTC","JNJ","JPM","KMI","KO","LLY","LMT","LOW","MA","MCD","MDLZ","MDT","MET","MMM","MO","MON","MRK","MS","MSFT","NEE","NKE"
+        ,"ORCL","OXY","PCLN","PEP","PFE","PG","PM","PYPL","QCOM","RTN","SBUX","SLB","SO","SPG","T","TGT","TWX","TXN","UNH","UNP","UPS","USB","USD","UTX","V","VZ","WBA","WFC","GME","AMC","TSLA","NVDA")
+
+
+}
